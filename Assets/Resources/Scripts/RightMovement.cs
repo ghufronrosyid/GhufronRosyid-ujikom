fa1d271fbeb613e1cd532540afce25f0a74506bb
ghufronrosyid <ghufronrosyid99@gmail.com>
2023-12-14 12:10:30 +0700

    Done

---
+++ Assets/Resources/Scripts/RightMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightMovement : MonoBehaviour
+{
+    public float speed = 1;
+    Rigidbody2D rigidbody2d;
+
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (rigidbody2d != null) {
+            rigidbody2d.velocity = transform.right * Time.deltaTime * speed;
+        }
+    }
+}
